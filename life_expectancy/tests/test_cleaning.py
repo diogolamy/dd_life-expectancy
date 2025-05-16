6e415b940cb677bd1fdf53bab606b3139bd57a70
diogolamy <diogolamy1999@gmail.com>
2025-05-16 18:18:14 +0100

    Reafactor cleaning.py and test_cleaning.py

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,15 +1,20 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import main
 from . import OUTPUT_DIR
 
 
-def test_clean_data(pt_life_expectancy_expected):
-    """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data(input_path="life_expectancy/data/eu_life_expectancy_raw.tsv",
-               output_path=OUTPUT_DIR / "pt_life_expectancy.csv",
-               country="PT")
+def test_main(pt_life_expectancy_expected):
+    """Run the 'main' function and compare the output to the expected output"""
+
+    args = [
+        "life_expectancy/data/eu_life_expectancy_raw.tsv",
+        str(OUTPUT_DIR / "pt_life_expectancy.csv"),
+        "--country", "PT"
+    ]
+
+    main(args)
 
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
